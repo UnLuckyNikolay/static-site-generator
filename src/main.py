17b075cd83423608b96b31d5b996e99eb4ab9062
UnLuckyNikolay <nickslast@gmail.com>
2025-06-03 14:58:13 +0200

    Add function to convert TextNode to HTMLNode

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 from textnode import *
+from leafnode import *
+from parentnode import *
 
 
 def main():
     textnode = TextNode("Testing this bs", TextType.LINK, "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     print(repr(textnode))
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.NORMAL:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text)
+        case TextType.IMAGE:
+            return LeafNode("img", text_node.text)
+        case _:
+            raise Exception("Unsupported TextType")
 
 
 if __name__ == "__main__":
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 from textnode import *
+from main import text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -17,6 +18,18 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Test", TextType.LINK, "https://guthib.com")
         node2 = TextNode("Test", TextType.LINK, "https://github.com")
         self.assertNotEqual(node, node2)
+
+    def test_text_to_html(self):
+        node = TextNode("Heyo", TextType.NORMAL)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.to_html(), "Heyo")
+
+    def test_text_to_html_bold(self):
+        node = TextNode("BIG", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.to_html(), "<b>BIG</b>")
 
 
 if __name__ == "__main__":
