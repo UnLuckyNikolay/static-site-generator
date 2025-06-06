c2dee8013507cca93e2cc6ca96e5e52a035e30fb
UnLuckyNikolay <nickslast@gmail.com>
2025-06-06 09:54:58 +0200

    Update split_nodes_delimiter function, add tests for it

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ from parentnode import *
 
 
 def main():
-    test = TextNode("WATCH OUT FOR THIS **NEW* TRICK!", TextType.TEXT)
-    nodes = split_nodes_delimiter([test], '**', TextType.BOLD)
+    test = TextNode("WATCH OUT FOR THIS **NEW** TRICK!", TextType.TEXT)
+    nodes = split_nodes_delimiter([test], '**')
     print(nodes)
 
 
@@ -27,20 +27,32 @@ def text_node_to_html_node(text_node):
             raise Exception("Unsupported TextType")
         
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter):
+    delimeters = ['`', '**', '_']
+    text_types = [TextType.CODE, TextType.BOLD, TextType.ITALIC]
     new_nodes = []
+    
+    if delimiter not in delimeters:
+        raise Exception(f"Invalid delimiter {delimiter}.")
+    
+    if isinstance(old_nodes, TextNode):
+        old_nodes = [old_nodes]
 
     for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT: # For now only splits plain text, if changed - remove test_not_plain_text in test_textnode.TestSplitNodes
+            new_nodes.append(old_node)
+            continue
+
         strings = old_node.text.split(delimiter)
         if len(strings) % 2 != 1:
             raise Exception(f"Missing closing delimiter {delimiter}. String: {old_node.text}")
 
         for i in range(len(strings)):
-            if len(strings[i]) == 0:
+            if len(strings[i]) == 0: # Remove this to keep empty nodes, also remove test_bold_at_the_start in test_textnode.TestSplitNodes
                 continue
 
             if i % 2 == 1:
-                new_nodes.append(TextNode(strings[i], text_type))
+                new_nodes.append(TextNode(strings[i], text_types[delimeters.index(delimiter)]))
             else:
                 new_nodes.append(TextNode(strings[i], TextType.TEXT))
 
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,47 @@
 import unittest
 from textnode import *
-from main import text_node_to_html_node
+from main import text_node_to_html_node, split_nodes_delimiter
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_missing_delimiter(self):
+        node = TextNode("Another one **bites the dust", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, [node], "**")
+
+    def test_invalid(self):
+        node = TextNode("Let's `finally` **test** _this_ shit", TextType.TEXT)
+        self.assertRaises(Exception, split_nodes_delimiter, [node], "whoops")
+
+    def test_code(self):
+        node = TextNode("Let's `finally` **test** _this_ shit", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "`")
+        self.assertEqual(TextNode("finally", TextType.CODE), nodes[1])
+
+    def test_italic(self):
+        node = TextNode("Let's `finally` **test** _this_ shit", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**")
+        self.assertEqual(TextNode("test", TextType.BOLD), nodes[1])
+
+    def test_bold(self):
+        node = TextNode("Let's `finally` **test** _this_ shit", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "_")
+        self.assertEqual(TextNode("this", TextType.ITALIC), nodes[1])
+
+    # Checks if enpty nodes are removed
+    def test_bold_at_the_start(self):
+        node = TextNode("**WE** ROCK!", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**")
+        self.assertEqual(TextNode("WE", TextType.BOLD), nodes[0])
+
+    def test_plain_text(self):
+        node = TextNode("Nice weather today", TextType.TEXT)
+        nodes = split_nodes_delimiter([node], "**")
+        self.assertEqual(node, nodes[0])
+
+    def test_not_plain_text(self):
+        node = TextNode("_Storm_ incoming!", TextType.BOLD)
+        nodes = split_nodes_delimiter([node], "_")
+        self.assertEqual(node, nodes[0])
 
 
 class TestTextNode(unittest.TestCase):
