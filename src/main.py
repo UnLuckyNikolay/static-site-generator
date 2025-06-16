2af79c5de11aab22c87e87b60866f2eec0dbd67c
UnLuckyNikolay <nickslast@gmail.com>
2025-06-16 14:46:25 +0200

    Add function generate_all_pages

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from functions_blocks import markdown_to_html_node
 public_dir = "./public"
 static_dir = "./static"
 content_dir = "./content"
+template_path = "./template.html"
 
 def main():
     if os.path.exists(public_dir):
@@ -13,18 +14,30 @@ def main():
 
     copy_file_tree(static_dir, public_dir)
 
-    generate_page("./content/index.md", "./template.html")
+    #generate_page("./content/index.md")
+    generate_all_pages()
 
 
 
-def generate_page(path_to_file, path_template):
+def generate_all_pages(subfolders=""):
+    dir = os.listdir(f"{content_dir}{subfolders}")
+
+    for file in dir:
+        if os.path.isdir(f"{content_dir}{subfolders}/{file}"):
+            os.mkdir(f"{public_dir}{subfolders}/{file}")
+            generate_all_pages((subfolders + f"/{file}"))
+        else:
+            generate_page(f"{content_dir}{subfolders}/{file}")
+
+
+def generate_page(path_to_file):
     path_file = path_to_file.removeprefix(content_dir)
     path = "/".join(path_file.split("/")[:-1])
     file_name = path_to_file.split("/")[-1]
     file_name = file_name.replace("md", "html")
-    print(f"Generating page from {content_dir}{path_file} to {public_dir}{path_file} using {path_template}")
+    print(f"Generating page from {content_dir}{path_file} to {public_dir}{path_file} using {template_path}")
 
-    with open(path_template) as f:
+    with open(template_path) as f:
         page = f.read()
     with open(path_to_file) as f:
         markdown = f.read()
@@ -34,8 +47,9 @@ def generate_page(path_to_file, path_template):
     page = page.replace("{{ Title }}", title)
     page = page.replace("{{ Content }}", html)
 
-    if not os.path.exists(f"{public_dir}{path}"):
-        os.makedirs(f"{public_dir}{path}")
+    # Done in generate_all_pages
+    #if not os.path.exists(f"{public_dir}{path}"):
+    #    os.makedirs(f"{public_dir}{path}")
 
     with open(f"{public_dir}{path}/{file_name}", "w") as page_file:
         page_file.write(page)
